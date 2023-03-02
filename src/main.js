4bc2ec6d81f44e81c946fd40719fa6026ca38db9
Gabriel Chazarreta <gfchazarreta@gmail.com>
2023-03-02 00:43:51 -0300

    Fixing errors

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createServer } from "./server.js";
 import config from "./config.js";
 
+const PORT = process.env.PORT || 8080;
+
 const app = createServer();
 try {
-  const connectedServer = await app.listen(config.PORT);
+  const connectedServer = await app.listen(PORT);
   console.log(
     `proceso #${process.pid} escuchando en el puerto ${
       connectedServer.address().port
